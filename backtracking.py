67c5ee96da517a6e17299457bb3377e09a1b0920
Jordan Franschman <147798646+JordanFranschman@users.noreply.github.com>
2025-10-30 21:24:17 -0400

    Finished backtracking file

--- backtracking.py
+++ backtracking.py
@@ -45,3 +45,68 @@ class Trail:
         
         for var,removed_vals in reversed(frame):
             domains[var] |= removed_vals
+
+
+def solve(csp: CSP) -> bool:
+    """
+    Solve CSP Sudoku puzzle using backtracking algorithm
+    
+    Argumentss:
+        csp: The constraint satisfaction problem to solve
+    Returns:
+        True if solution found, false otherwise
+    """
+    trail = Trail()
+    return _backtrack(csp, trail)
+
+
+def _backtrack(csp: CSP, trail:Trail) -> bool:
+    """Recursive backtracking helper func"""
+    
+    if csp.is_solved():
+        return True
+    
+    var = heuristics.select_var_mrv(csp)
+    if var is None:
+        return True
+    values = heuristics.order_values_lcv(csp, var)
+    
+    for value in values:
+        trail.push_frame()
+        if _assign_and_infer(csp,var, value, trail):
+            if _backtrack(csp, trail):
+                return True
+        
+        trail.pop_frame_and_undo(csp.domains)
+    
+    return False
+
+
+def _assign_and_infer(csp: CSP, var: Var, value: int, trail: Trail) -> bool:
+    """
+    Assign a value to variable then run AC-3 inference,
+    returns False if inconsistency
+    """
+    
+    saved_domains = {}
+    for v in csp.variables:
+        saved_domains[v] = csp.domains[v].copy()
+    
+    domain = csp.domains[var]
+    domain.clear()
+    domain.add(value)
+    
+    arcs = [(xj,var) for xj in csp.neighbors[var]]
+    is_consistent, _ = ac3.ac3(csp, queue=arcs, track_queue=False)
+    
+    if not is_consistent:
+        return False
+    
+    for v in csp.variables:
+        
+        removed = saved_domains[v] - csp.domains[v]
+        
+        if removed:
+            trail.record(v, removed)
+            
+    return True
