c1ad4f1e133773294bd7b57046817eee35e7b494
Jordan Franschman <147798646+JordanFranschman@users.noreply.github.com>
2025-10-30 21:03:19 -0400

    Finished Trail class
    Added methods for managing backtracking frames.

--- backtracking.py
+++ backtracking.py
@@ -27,3 +27,21 @@ class Trail:
     
     def __init__(self):
         self.frames: List[List[tuple[Var,Set[int]]]] = []
+
+    def push_frame(self):
+        """Start new backtracking frame"""
+        self.frames.append([])
+    
+    def record(self, var: Var, removed_values: Set[int]):
+        """Record vals removed from a specific variables domain"""
+        if removed_value and self.frames:
+            self.frames[-1].append((var,removed_values.copy())
+    
+    def pop_frame_and_undo(self, domains: Dict[Var, Set[int]]):
+        """Undo all changes in the current frame (backtrack!)"""
+        if not self.frames:
+            return
+        frame = self.frames.pop()
+        
+        for var,removed_vals in reversed(frame):
+            domains[var] |= removed_vals
